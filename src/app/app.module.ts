5ec96b748275e2c6a0ff1979a034e5e949967d54
StevenSerrien <stevenserrien@gmail.com>
2019-06-13 11:41:21 +0200

    4 files modified, 8 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HotelCardComponent } from '../hotel-card/hotel-card.component';
+import { StarComponent } from '../hotel-card/star.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HotelCardComponent, StarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
